dac887b07ebe6e31e0dadab85076d2a2d969950c
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-10 09:07:52 +1100

    2d animation

--- swe_2d_animation.py
+++ swe_2d_animation.py
@@ -3,29 +3,62 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import matplotlib
+from solve import dumptimeintegrate
 
-N = 150 # Meshsize
-fps = 10 # frame per sec
-frn = 50 # frame number of the animation
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
 
-x = np.linspace(-4,4,N+1)
-x, y = np.meshgrid(x, x)
-zarray = np.zeros((N+1, N+1, frn))
+## initialise parameters: domain, coefficients etc
+lx = 10
+ly = 10  ## domain length
+nx = 201
+ny = 201
+u0 = 2
+v0 = 0
+g = 9.8
+H = 1
 
-f = lambda x,y,sig : 1/np.sqrt(sig)*np.exp(-(x**2+y**2)/sig**2)
+X = np.linspace(0, lx, nx)
+dx = X[1] - X[0]
+Y = np.linspace(0, ly, ny)
+dy = Y[1] - Y[0]
+x, y = np.meshgrid(X, Y)
+u = 0 * x + 0 * y
+v = 0 * x + 0 * y
+harg = -((x - 5) ** 2 + (y - 5) ** 2) / 0.3
+h = 10 * np.exp(harg)
 
-for i in range(frn):
-    zarray[:, :, i] = f(x, y, 1.5 + np.sin(i * 2 * np.pi / frn))
+cfl = 0.5  # CFL number
+stepS = np.sqrt(dx ** 2 + dy ** 2)
+dt = cfl * stepS / (u0 + math.sqrt(g * H))
+tend = 20
+nt = int(round(tend / dt))  # number of time steps
+n = 0  # counter
+t = 0
+# method = "Euler"
+# method = "RK2"
+method = "RK4"
+order = 6
+
+out, times = dumptimeintegrate(x, y, nx, ny, dx, dy, order, u0, v0, H, g, 5, dt, method)
+zarray = np.array(out)
+
+print(zarray.shape)
 
 def update_plot(frame_number, zarray, plot):
     plot[0].remove()
-    plot[0] = ax.plot_surface(x, y, zarray[:, :, frame_number], cmap="magma")
+    plot[0] = ax.plot_surface(x, y, zarray[frame_number, 0, :, :], cmap="magma")
+
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection="3d")
 
 
-plot = [ax.plot_surface(x, y, zarray[:, :, 0], color='0.75', rstride=1, cstride=1)]
-ax.set_zlim(0,1.1)
-ani = animation.FuncAnimation(fig, update_plot, frn, fargs=(zarray, plot), interval=1000 / fps)
+plot = [ax.plot_surface(x, y, zarray[0, 0, :, :], color="0.75", rstride=1, cstride=1)]
+ax.set_zlim(-2, 10)
+ani = animation.FuncAnimation(
+    fig, update_plot, len(out), fargs=(zarray, plot), interval=10
+)
 plt.show()
